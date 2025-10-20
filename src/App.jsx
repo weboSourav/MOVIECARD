2b188f37621a38c016cb58d0f97e589b5b0e6611
unknown <winterfell118@gmail.com>
2025-10-20 23:42:14 +0530

    new website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import MovieCard from "./components/MovieCard"
+
+function App (){
+
+return (
+
+<div>
+
+<MovieCard movie={{title:"dhamaal",release_date:"2024"}}/>
+</div>
+
+
+
+
+
+
+)
+}
+
+export default App
